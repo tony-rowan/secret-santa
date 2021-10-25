96009c6a7420b4af2dea9fd5ed14cd9efe6d5083
Tony Rowan <trowan812@gmail.com>
2021-10-25 20:10:06 +0100

    Build tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,12 @@
-import "./application.css";// This file is automatically compiled by Webpack, along with any other files
+import "./application.css";
+
+// This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+import Rails from "@rails/ujs";
 
-Rails.start()
+Rails.start();
 
-import "controllers"
+import "controllers";
