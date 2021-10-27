09fec2d8e397f7cf5117c0f2b642e0cc5477c4c8
Tony Rowan <trowan812@gmail.com>
2021-10-27 13:29:41 +0100

    Actual landing page

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,9 @@
+class RootController < ApplicationController
+  before_action :redirect_logged_in_users_to_dashboard
+
+  private
+
+  def redirect_logged_in_users_to_dashboard
+    redirect_to dashboard_path if logged_in?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'dashboards#show'
+  root 'root#show'
 
-  resource :sessions, only: %i[new create destroy]
-
+  resource :dashboard, only: %i[show]
+  resource :session, only: %i[new create destroy]
   resources :users, only: %i[new create show edit update destroy]
   resources :ideas, only: %i[new create destroy]
   resources :groups, only: %i[new create show edit update destroy]
-  resources :invites, only: %i[show update]
 end
