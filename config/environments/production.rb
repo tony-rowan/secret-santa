ecd6196d678f0eaf985a506a0c24b54569ec318c
Tony Rowan <trowan812@gmail.com>
2022-10-19 19:13:08 +0100

    Revert "Allow ENV to specify an allowed host"
    This reverts commit 4fa65128bb416096d8d008bb2942e345b230627d.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Allow environemnt to specify the host name
-  config.hosts << ENV["ALLOW_HOST"]
 end
