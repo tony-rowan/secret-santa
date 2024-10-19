26426848ee2faf716329b739fb6f5b35e3981172
Tony Rowan <trowan812@gmail.com>
2024-10-19 17:48:31 +0100

    Add missing up route from upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :ideas, only: %i[create destroy]
   resources :groups, only: %i[new create show edit update destroy]
   resources :invites, only: %i[show update]
+
+  get "up" => "rails/health#show", :as => :rails_health_check
 end
